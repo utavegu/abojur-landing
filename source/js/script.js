a7b51c2129b66f49b6855582da0af9a8c2cf9c32
Utavegu <utavegu@list.ru>
2021-01-04 16:18:05 +0500

    Реализовал умный якорь

--- source/js/script.js
+++ source/js/script.js
@@ -9,4 +9,43 @@ menuBurgerButton.addEventListener("click", (evt) => {
 })
 
 
+
+/* Anchor script */
+
+const anchor = document.querySelector('.anchor');
+
+const showAnchor = () => {
+  if (window.pageYOffset > document.documentElement.clientHeight) {
+    anchor.classList.add('anchor-show');
+  } else {
+    anchor.classList.remove('anchor-show');
+  }
+}
+
+let coords;
+
+const go = () => {
+  if (anchor.classList.contains("anchor-downward")) {
+    window.scrollTo({
+      top: coords,
+      behavior: "smooth"
+    });
+    anchor.classList.remove("anchor-downward");
+  }
+  else {
+    coords = window.pageYOffset;
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+    anchor.classList.add("anchor-downward");
+    window.removeEventListener('scroll', showAnchor);
+  }
+}
+
+window.addEventListener('scroll', showAnchor);
+anchor.addEventListener('click', go);
+
+
+
 /* *** script */
