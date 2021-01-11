dfdb06d17d8b91c8112c74bb85f09c9d8e46f6b3
Utavegu <utavegu@list.ru>
2021-01-11 18:09:44 +0500

    Оживил стрелочки слайдера

--- source/js/script.js
+++ source/js/script.js
@@ -122,3 +122,32 @@ modalConsultationTraps.forEach(element => element.addEventListener("focus", () =
   }
 }));
 
+
+
+/* SLIDER SCRIPT */
+
+const carousel = document.querySelector(".solutions__slider");
+
+let width = 960; // ширина картинки
+let count = 1; // видимое количество изображений
+
+let list = carousel.querySelector('ul');
+let listElems = carousel.querySelectorAll('li');
+
+let position = 0; // положение ленты прокрутки
+
+carousel.querySelector('.solutions__slider-btn--prev').onclick = function() {
+  // сдвиг влево
+  position += width * count;
+  // последнее передвижение влево может быть не на 3, а на 2 или 1 элемент
+  position = Math.min(position, 0)
+  list.style.marginLeft = position + 'px';
+};
+
+carousel.querySelector('.solutions__slider-btn--next').onclick = function() {
+  // сдвиг вправо
+  position -= width * count;
+  // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
+  position = Math.max(position, -width * (listElems.length - count));
+  list.style.marginLeft = position + 'px';
+};
